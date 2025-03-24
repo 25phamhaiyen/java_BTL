0e16dfd4747ec09caf505d1929b229fd343c89d6
25phamhaiyen <145901112+25phamhaiyen@users.noreply.github.com>
2025-03-24 15:47:48 +0700

    cập nhật DataBase

--- mySQL/setUp.sql
+++ mySQL/setUp.sql
@@ -9,28 +9,6 @@ USE bestpets;
 -- Cài đặt
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
--- 				Tạo bảng Khách hàng
-CREATE TABLE `customer` (
-	`customer_ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`lastName` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`firstName` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`phoneNumber` VARCHAR(10) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`sex` BIT(1) NOT NULL,
-	`citizenNumber` VARCHAR(12) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`address` TEXT NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`AccountID` INT UNSIGNED NOT NULL,
-	PRIMARY KEY (`customer_ID`) USING BTREE,
-	UNIQUE INDEX `Customer_PhoneNumber` (`phoneNumber`) USING BTREE,
-	UNIQUE INDEX `Customer_CitizenNumber` (`citizenNumber`) USING BTREE,
-	INDEX `AccountID` (`AccountID`) USING BTREE,
-	CONSTRAINT `FK_customer_account` FOREIGN KEY (`AccountID`) REFERENCES `account` (`AccountID`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT `CkCustomer_CitizenNumber` CHECK ((length(`citizenNumber`) = 12)),
-	CONSTRAINT `CkCustomer_phoneNumber` CHECK ((length(`phoneNumber`) = 10))
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
 
 
 
@@ -74,31 +52,7 @@ ENGINE=InnoDB
 AUTO_INCREMENT=1
 ;
 
--- 				Tạo bảng Nhân viên
-CREATE TABLE `staff` (
-	`StaffID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`lastName` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`firstName` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`Sex` BIT(1) NOT NULL,
-	`phoneNumber` VARCHAR(10) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`CitizenNumber` VARCHAR(12) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`Address` VARCHAR(300) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`Role_ID` INT UNSIGNED NOT NULL,
-	`AccountID` INT UNSIGNED NOT NULL,
-	PRIMARY KEY (`StaffID`) USING BTREE,
-	UNIQUE INDEX `UnStaff_UN_PhoneNumber` (`phoneNumber`) USING BTREE,
-	UNIQUE INDEX `UnStaff_UN_CitizenNumber` (`CitizenNumber`) USING BTREE,
-	INDEX `FkStaff_RoleID` (`Role_ID`) USING BTREE,
-	INDEX `AccountID` (`AccountID`) USING BTREE,
-	CONSTRAINT `FkStaff_RoleID` FOREIGN KEY (`Role_ID`) REFERENCES `role` (`Role_ID`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT `FK_staff_account` FOREIGN KEY (`AccountID`) REFERENCES `account` (`AccountID`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT `CkStaff_UN_CitizenNumber` CHECK ((length(`CitizenNumber`) = 12)),
-	CONSTRAINT `CkStaff_UN_PhoneNumber` CHECK ((length(`phoneNumber`) = 10))
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
+
 
 --				Tạo bảng loại dịch vụ
 CREATE TABLE `typeservice` (
@@ -127,6 +81,118 @@ COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=1
 ;
+
+
+-- 				Tạo bảng Tài khoản
+CREATE TABLE `account` (
+	`AccountID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`UN_Username` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Password` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Email` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Role_ID` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`AccountID`) USING BTREE,
+	UNIQUE INDEX `UnAccount_UN_Username` (`UN_Username`) USING BTREE,
+	INDEX `Role_ID` (`Role_ID`) USING BTREE,
+	CONSTRAINT `FK_account_role` FOREIGN KEY (`Role_ID`) REFERENCES `role` (`Role_ID`) ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
+-- 				Tạo bảng Khách hàng
+CREATE TABLE `customer` (
+	`customer_ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`lastName` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`firstName` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`phoneNumber` VARCHAR(10) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`sex` TINYINT NOT NULL,
+	`citizenNumber` VARCHAR(12) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`address` TEXT NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`AccountID` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`customer_ID`) USING BTREE,
+	UNIQUE INDEX `Customer_PhoneNumber` (`phoneNumber`) USING BTREE,
+	UNIQUE INDEX `Customer_CitizenNumber` (`citizenNumber`) USING BTREE,
+	INDEX `AccountID` (`AccountID`) USING BTREE,
+	CONSTRAINT `FK_customer_account` FOREIGN KEY (`AccountID`) REFERENCES `account` (`AccountID`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `CkCustomer_CitizenNumber` CHECK ((length(`citizenNumber`) = 12)),
+	CONSTRAINT `CkCustomer_phoneNumber` CHECK ((length(`phoneNumber`) = 10))
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
+
+-- 				Tạo bảng Nhân viên
+CREATE TABLE `staff` (
+	`StaffID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`lastName` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`firstName` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Sex` TINYINT NOT NULL,
+	`phoneNumber` VARCHAR(10) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`CitizenNumber` VARCHAR(12) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Address` VARCHAR(300) NOT NULL COLLATE 'utf8mb4_unicode_ci',
+	`Role_ID` INT UNSIGNED NOT NULL,
+	`AccountID` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`StaffID`) USING BTREE,
+	UNIQUE INDEX `UnStaff_UN_PhoneNumber` (`phoneNumber`) USING BTREE,
+	UNIQUE INDEX `UnStaff_UN_CitizenNumber` (`CitizenNumber`) USING BTREE,
+	INDEX `FkStaff_RoleID` (`Role_ID`) USING BTREE,
+	INDEX `AccountID` (`AccountID`) USING BTREE,
+	CONSTRAINT `FkStaff_RoleID` FOREIGN KEY (`Role_ID`) REFERENCES `role` (`Role_ID`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `FK_staff_account` FOREIGN KEY (`AccountID`) REFERENCES `account` (`AccountID`) ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT `CkStaff_UN_CitizenNumber` CHECK ((length(`CitizenNumber`) = 12)),
+	CONSTRAINT `CkStaff_UN_PhoneNumber` CHECK ((length(`phoneNumber`) = 10))
+)
+COLLATE='utf8mb4_unicode_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
+
+-- 				Tạo bảng Đơn hàng
+CREATE TABLE `order` (
+    `orderID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `orderDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Ngày đặt hàng
+    `appointmentDate` DATETIME NULL, -- Ngày khách hẹn
+    `orderType` ENUM('AtStore', 'Appointment') NOT NULL DEFAULT 'AtStore', -- Loại đơn
+    `Total` DOUBLE NOT NULL DEFAULT 0,
+    `Customer_ID` INT UNSIGNED NOT NULL,
+    `StaffID` INT UNSIGNED NULL,
+    `PaymentStatusID` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`orderID`),
+    INDEX `Fkorder_Customer_ID` (`Customer_ID`),
+    INDEX `Fkorder_StaffID` (`StaffID`),
+    INDEX `Fkorder_PaymentStatusID` (`PaymentStatusID`),
+    CONSTRAINT `Fkorder_Customer_ID` FOREIGN KEY (`Customer_ID`) REFERENCES `customer` (`customer_ID`) ON DELETE CASCADE,
+    CONSTRAINT `Fkorder_StaffID` FOREIGN KEY (`StaffID`) REFERENCES `staff` (`StaffID`) ON DELETE CASCADE,
+    CONSTRAINT `Fkorder_PaymentStatusID` FOREIGN KEY (`PaymentStatusID`) REFERENCES `paymentstatus` (`PaymentStatusID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- Tạo bảng hóa đơn (invoice)
+CREATE TABLE `invoice` (
+    `InvoiceID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `OrderID` INT UNSIGNED NOT NULL,
+    `TotalAmount` DOUBLE NOT NULL DEFAULT 0,
+    `CreatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`InvoiceID`),
+    UNIQUE INDEX `UnInvoice_OrderID` (`OrderID`),
+    INDEX `FkInvoice_OrderID` (`OrderID`),
+    CONSTRAINT `FkInvoice_OrderID` FOREIGN KEY (`OrderID`) REFERENCES `order`(`orderID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=UTF8MB4_UNICODE_CI;
+-- Chi tiết HD
+CREATE TABLE `invoice_detail` (
+    `InvoiceDetailID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `InvoiceID` INT UNSIGNED NOT NULL,
+    `ServiceID` INT UNSIGNED NOT NULL,
+    `Quantity` INT UNSIGNED NOT NULL,
+    `UnitPrice` DOUBLE NOT NULL,
+    `TotalPrice` DOUBLE GENERATED ALWAYS AS (Quantity * UnitPrice) STORED,
+    PRIMARY KEY (`InvoiceDetailID`),
+    INDEX `FkInvoiceDetail_InvoiceID` (`InvoiceID`),
+    INDEX `FkInvoiceDetail_ServiceID` (`ServiceID`),
+    CONSTRAINT `FkInvoiceDetail_InvoiceID` FOREIGN KEY (`InvoiceID`) REFERENCES `invoice`(`InvoiceID`) ON DELETE CASCADE,
+    CONSTRAINT `FkInvoiceDetail_ServiceID` FOREIGN KEY (`ServiceID`) REFERENCES `service`(`serviceID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 -- 				Tạo bảng loại thú cưng
@@ -159,70 +225,17 @@ COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=1
 ;
-
--- 				Tạo bảng Đơn hàng
-CREATE TABLE `order` (
-	`orderID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`orderDate` DATE NOT NULL,
-	`Total` DOUBLE NOT NULL DEFAULT '0',
-	`Customer_ID` INT UNSIGNED NOT NULL,
-	`StaffID` INT UNSIGNED NOT NULL,
-	PRIMARY KEY (`orderID`) USING BTREE,
-	INDEX `Fkorder_Customer_ID` (`Customer_ID`) USING BTREE,
-	INDEX `Fkorder_StaffID` (`StaffID`) USING BTREE,
-	CONSTRAINT `Fkorder_Customer_ID` FOREIGN KEY (`Customer_ID`) REFERENCES `customer` (`customer_ID`) ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT `Fkorder_StaffID` FOREIGN KEY (`StaffID`) REFERENCES `staff` (`StaffID`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
-
-
--- 				Tạo bảng Order
--- CREATE TABLE `Order`(
--- 	PartyID INT UNSIGNED,
---    DishID INT UNSIGNED,
---    Price DOUBLE NOT NULL,
---    CONSTRAINT PkOrder_PartyID_DishID PRIMARY KEY (PartyID, DishID),
---    CONSTRAINT FkOrder_PartyID FOREIGN KEY (PartyID) REFERENCES Party(PartyID),
---    CONSTRAINT FkOrder_DishID FOREIGN KEY (DishID) REFERENCES Dish(DishID)
--- );
-
--- 				Tạo bảng DetailInvoice
--- CREATE TABLE DetailInvoice(
---	DetailInvoiceID INT UNSIGNED AUTO_INCREMENT,
---    DishName VARCHAR(255) NOT NULL,
---    Unit_Price DOUBLE NOT NULL,
---    `Number` TINYINT UNSIGNED,
---    Amount DOUBLE NOT NULL,
---    InvoiceID INT UNSIGNED,
---    CONSTRAINT PkDetailInvoice_DetailInvoiceID PRIMARY KEY (DetailInvoiceID),
---    CONSTRAINT FkDetailInvoice_InvoiceID FOREIGN KEY (InvoiceID) REFERENCES Invoice(InvoiceID)
--- );customer
--- 				Tạo bảng Work
--- CREATE TABLE work(
---	PartyID INT UNSIGNED,customer
---    StaffID INT UNSIGNED,
---    Salary DOUBLE DEFAULT 0,
---    CONSTRAINT PkWork_PartyID_StaffID PRIMARY KEY (PartyID,StaffID),
---    CONSTRAINT FkWork_DishID FOREIGN KEY (PartyID) REFERENCES Party(PartyID),
---    CONSTRAINT FkWork_InvoiceID FOREIGN KEY (StaffID) REFERENCES Staff(StaffID)
--- );
-
--- 				Tạo bảng Tài khoản
-CREATE TABLE `account` (
-	`AccountID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`UN_Username` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`Password` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_unicode_ci',
-	`Email` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
-	`Role_ID` INT UNSIGNED NOT NULL,
-	PRIMARY KEY (`AccountID`) USING BTREE,
-	UNIQUE INDEX `UnAccount_UN_Username` (`UN_Username`) USING BTREE,
-	INDEX `Role_ID` (`Role_ID`) USING BTREE,
-	CONSTRAINT `FK_account_role` FOREIGN KEY (`Role_ID`) REFERENCES `role` (`Role_ID`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-COLLATE='utf8mb4_unicode_ci'
-ENGINE=InnoDB
-AUTO_INCREMENT=1
-;
+-- Tạo bảng chi tiết đơn hàng (order_detail)
+CREATE TABLE `order_detail` (
+    `OrderDetailID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `OrderID` INT UNSIGNED NOT NULL,
+    `ServiceID` INT UNSIGNED NOT NULL,
+    `Quantity` INT UNSIGNED NOT NULL DEFAULT 1,
+    `UnitPrice` DOUBLE NOT NULL,
+    `TotalPrice` DOUBLE GENERATED ALWAYS AS (Quantity * UnitPrice) STORED,
+    PRIMARY KEY (`OrderDetailID`),
+    INDEX `FkOrderDetail_OrderID` (`OrderID`),
+    INDEX `FkOrderDetail_ServiceID` (`ServiceID`),
+    CONSTRAINT `FkOrderDetail_OrderID` FOREIGN KEY (`OrderID`) REFERENCES `order`(`orderID`) ON DELETE CASCADE,
+    CONSTRAINT `FkOrderDetail_ServiceID` FOREIGN KEY (`ServiceID`) REFERENCES `service`(`serviceID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
