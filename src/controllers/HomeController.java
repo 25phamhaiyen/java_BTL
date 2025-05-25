9ae35be1cbec26d976a120f2a880e2c1d7e07655
Tich-ne1625 <vuongtieulam10062005@gmail.com>
2025-05-25 17:26:25 +0700

    hoàn chỉnh giao diện home , login , staff home dùng song ngữ

--- src/controllers/HomeController.java
+++ src/controllers/HomeController.java
@@ -3,43 +3,54 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import utils.I18nUtil;
+import utils.LanguageManager;
+import java.util.Locale;
 
-public class HomeController {
+public class HomeController implements I18nUtil.I18nUpdatable {
+    @FXML
+    private Label title;
+    @FXML
+    private Button btnLogin, btnLanguage;
 
-	@FXML
-	private Label title;
-	@FXML
-	private Button btnLogin, btnLanguage;
-	private boolean isEnglish = false;
+    @FXML
+    public void initialize() {
+        // Register for language updates
+        I18nUtil.register(this);
+        
+        // Initial language setup
+        updateLanguage();
+        
+        // Set up event handlers
+        btnLogin.setOnAction(this::handleLogin);
+        btnLanguage.setOnAction(this::toggleLanguage);
+    }
+    
+    @Override
+    public void updateLanguage() {
+        LanguageManager langManager = LanguageManager.getInstance();
+        
+        // Update text based on current language
+        title.setText(langManager.getString("home.title"));
+        btnLogin.setText(langManager.getString("home.login"));
+        btnLanguage.setText(langManager.getString("home.language"));
+    }
 
-	@FXML
-	public void initialize() {
-		updateLanguage();
-		btnLogin.setOnAction(this::handleLogin);
-		btnLanguage.setOnAction(this::toggleLanguage);
-	}
+    private void handleLogin(ActionEvent event) {
+        SceneSwitcher.switchScene("login.fxml");
+    }
 
-	private void handleLogin(ActionEvent event) {
-		SceneSwitcher.switchScene("login.fxml");
-	}
-
-	private void toggleLanguage(ActionEvent event) {
-		isEnglish = !isEnglish;
-		updateLanguage();
-	}
-
-	private void updateLanguage() {
-		if (isEnglish) {
-			title.setText("PET CARE");
-			btnLogin.setText("Login");
-			btnLanguage.setText("EN|VN");
-		} else {
-			title.setText("CHĂM SÓC THÚ CƯNG CỦA BẠN");
-			btnLogin.setText("Đăng nhập");
-			btnLanguage.setText("VN|GB");
-		}
-	}
+    private void toggleLanguage(ActionEvent event) {
+        LanguageManager langManager = LanguageManager.getInstance();
+        Locale currentLocale = langManager.getCurrentLocale();
+        
+        // Toggle between Vietnamese and English
+        if (currentLocale.equals(LanguageManager.VIETNAMESE)) {
+            I18nUtil.switchLanguage(LanguageManager.ENGLISH);
+        } else {
+            I18nUtil.switchLanguage(LanguageManager.VIETNAMESE);
+        }
+    }
 }
